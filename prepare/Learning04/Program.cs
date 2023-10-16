6bd7cf5b0f83265d9be993289b3767b39b52aa7d
ambonani <refilwe.given@gmail.com>
2023-10-16 12:35:34 +0200

    added maths and writting assignments

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Kano Mbonani", "Fractions");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Kabo Mbonani", "Addition", "8.1", "7-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Gugu Sibiya", "African Studies", "The Rise of the Zulu Kindom");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
