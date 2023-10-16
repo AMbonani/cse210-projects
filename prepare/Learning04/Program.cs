0c726f24f31e07d8299e00b28869da99418c9bd5
ambonani <refilwe.given@gmail.com>
2023-10-16 12:42:58 +0200

    updated L04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a base "Assignment" object
+       
         Assignment a1 = new Assignment("Kano Mbonani", "Fractions");
         Console.WriteLine(a1.GetSummary());
 
-        // Now create the derived class assignments
+        
         MathAssignment a2 = new MathAssignment("Kabo Mbonani", "Addition", "8.1", "7-10");
         Console.WriteLine(a2.GetSummary());
         Console.WriteLine(a2.GetHomeworkList());
